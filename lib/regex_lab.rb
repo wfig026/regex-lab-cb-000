58dbc76c0fa3d49ddb631738329843c6a17aaca4
Will Figueiredo <will@wfigueiredo.com>
2018-09-02 20:56:07 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-  word.match(/\A[aeiouAEIOU]/)
+  if word.match(/\A[aeiouAEIOU]/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -11,9 +15,17 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\A[A-Z].+[.,?!]\z/)
+  if text.match(/\A[A-Z].+[.,?!]\z/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/\d/).length == 10
+  if phone.scan(/\d/).length == 10
+    true
+  else
+    false
+  end
 end
